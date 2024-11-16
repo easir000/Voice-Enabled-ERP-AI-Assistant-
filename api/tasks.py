0f256d8039aac9a7ca45c9a19412cae6de9c0261
saturated <46495303+easir000@users.noreply.github.com>
2024-11-16 17:20:20 +0600

    Deletea l

--- api/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from django.utils import timezone
-from . models import Book
-
-
-@shared_task
-def archive_old_books():
-    ten_years_ago = timezone.now().date() - timezone.timedelta(days=365 * 10)
-    Book.objects.filter(published_date___lte=ten_years_ago).update(is_archive=True)
